29a2dd454763678f81a93a3c87b999826fd62811
bordershot <bordershot@yahoo.com>
2016-05-06 23:15:16 -0700

    Added argparse to reuseSSH.py

--- reuseSSH.py
+++ reuseSSH.py
@@ -10,10 +10,14 @@ import sys
 #import checkHost
 from checkHost import ssh_to_host
 import re
+import argparse
 
 def main():
+    parser = argparse.ArgumentParser(description='Check packages on remote system.')
+    parser.add_argument('host', help='Hostname to check')
+    args = parser.parse_args()
 #    print(help(ssh_to_host))
-    output = ssh_to_host('tardis', 'ls -lSh /var/log/')
+    output = ssh_to_host(args.host, 'ls -lSh /var/log/')
 #    print(output)
     parse_output = re.findall(r'-\S+\s+\S+\s+\S+\s+\S+\s+(\S+)\s+\S+\s+\S+\s+\S+\s+(\S+)',output)
     print('5 largest log files on tardis:')
