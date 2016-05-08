483eb6d85f4b9a84362cd0baf8448092cc2619c4
bordershot <bordershot@yahoo.com>
2016-05-08 14:10:59 -0700

    Pep8 fixes for graphiteclient.py

--- graphiteclient.py
+++ graphiteclient.py
@@ -1,13 +1,15 @@
 import sys
 import socket
 
+
 def upload_to_graphite(message, server='localhost', port=2003, proto='udp'):
-    #check message?
+    # add a check for message formatting?
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
         try:
-            s.connect((server, port)) #function takes tuple of (servername, port)
+            s.connect((server, port))   # tuple of (servername, port)
             s.sendall(message.encode('ascii'))
             s.close()
         except:
-            sys.stderr.write('ERROR: Could not write to Graphite ' + str(sys.exc_info()[0]))
+            sys.stderr.write('ERROR: Could not write to Graphite ' +
+                             str(sys.exc_info()[0]))
             sys.exit(1)
