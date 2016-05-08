9fe09ce9a3fa063750e97e9a3aa2a047cc0b47f6
bordershot <bordershot@yahoo.com>
2016-05-08 14:01:40 -0700

    Pep8 fixes

--- rest-json.py
+++ rest-json.py
@@ -1,31 +1,32 @@
 #!/usr/bin/python3
 
 """
-Demonstration of REST API, retrieve json, parse json.
+#Demonstration of REST API, retrieve json, parse json.
 """
 
 import sys
 import requests
 import json
 
+
 def main():
     root = 'http://jsonplaceholder.typicode.com/'
     section = 'comments'
     url = root + section
-    payload = { 'userId':'1' }
+    payload = {'userId': '1'}
     r = requests.get(url, params=payload)
     print('get: ' + str(r.status_code))
 #    print(r.text)
 #    print(r.json())
     section = 'posts/1'
     url = root + section
-    payload = { 'id':1, 'title':'foo', 'body':'bar', 'userId':1 }
+    payload = {'id': 1, 'title': 'foo', 'body': 'bar', 'userId': 1}
     r = requests.put(url, data=payload)
     print('put: ' + str(r.status_code))
 #    print(r.text)
     section = 'posts'
     url = root + section
-    payload = { 'title':'foo', 'body':'bar', 'userId':92 }
+    payload = {'title': 'foo', 'body': 'bar', 'userId': 92}
     r = requests.post(url, data=payload)
     print('post: ' + str(r.status_code))
 #    print(r.json())
@@ -35,4 +36,4 @@ def main():
     print(json.dumps(output))
 
 if __name__ == '__main__':
-      main()
+    main()
