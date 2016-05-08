288e736e7891e0a23f41cedfd98b6c0c90017fb5
bordershot <bordershot@yahoo.com>
2016-05-08 13:58:13 -0700

    Pep8 fixes

--- reuseSSH.py
+++ reuseSSH.py
@@ -1,28 +1,33 @@
 #!/usr/bin/python3
 """
-Dimension requirements fulfilled:
-Project: Reuse SSH to host, run command, parse output, return 5 largest log files
-* import function from checkHost.py
+#Dimension requirements fulfilled:
+#Project: Reuse SSH to host, run command, parse output,
+#         return 5 largest log files
+#* import function from checkHost.py
 """
 
 
 import sys
-#import checkHost
 from checkHost import ssh_to_host
 import re
 import argparse
 
+
 def main():
-    parser = argparse.ArgumentParser(description='Check packages on remote system.')
+    parser = argparse.ArgumentParser(
+        description='Check packages on remote system.'
+        )
     parser.add_argument('host', help='Hostname to check')
     args = parser.parse_args()
 #    print(help(ssh_to_host))
     output = ssh_to_host(args.host, 'ls -lSh /var/log/')
 #    print(output)
-    parse_output = re.findall(r'-\S+\s+\S+\s+\S+\s+\S+\s+(\S+)\s+\S+\s+\S+\s+\S+\s+(\S+)',output)
+    parse_output = re.findall(
+        r'-\S+\s+\S+\s+\S+\s+\S+\s+(\S+)\s+\S+\s+\S+\s+\S+\s+(\S+)', output
+        )
     print('5 largest log files on ' + args.host + ':')
     for file in parse_output[:5]:
-        print(file[0] + '\t' +  file[1])
+        print(file[0] + '\t' + file[1])
 
 if __name__ == '__main__':
-      main()
+    main()
