727ed9927e34c5d37e569d6f8b1e299778996c24
bordershot <bordershot@yahoo.com>
2016-05-08 15:02:46 -0700

    Pep8 formatting fixes

--- checkHost.py
+++ checkHost.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
 """
-SSH to host(s), check for installed packages 
+SSH to host(s), check for installed packages
 
 Dimension requirements fulfilled:
     Project: SSH to host, run command, parse output
@@ -14,13 +14,13 @@ Dimension requirements fulfilled:
     * argparse:
         * Required and optional arguments.
         * Display usage on error input.
-
 """
 
 import sys
 import subprocess
 import argparse
 import re
+
 
 def ssh_to_host(hostname, command):
     """
@@ -32,24 +32,27 @@ def ssh_to_host(hostname, command):
         sys.stderr.write('ERROR: SSH unsuccessful to ' + hostname + '\n')
     return output
 
+
 def parse_dpkg(output):
     package_list = re.findall(r'ii\s+(\S+)\s+', output)
 #    print(package_list)
     return package_list
 
 
-
 def main():
-    parser = argparse.ArgumentParser(description='Check packages on remote system.')
-    parser.add_argument('host', action='append', help='Hostname to check, default to localhost')
-    parser.add_argument('-p','--package', action='append', help='Packages to find, default to ouput all')
+    parser = argparse.ArgumentParser(
+        description='Check packages on remote system.')
+    parser.add_argument('host', action='append',
+                        help='Hostname to check, default to localhost')
+    parser.add_argument('-p', '--package',
+                        action='append',
+                        help='Packages to find, default to ouput all')
     args = parser.parse_args()
     if not args.host:
-        args.host = [ 'localhost' ]
+        args.host = ['localhost']
 
 #    print(args.host)
 #    print(args.package)
-#    packages_to_find = [ 'apache', 'apache2', 'grafana', 'collectd', 'non-existant-package' ]
 
     for host in args.host:
         dpkg_out = ssh_to_host(host, 'dpkg -l')
@@ -62,19 +65,10 @@ def main():
                 continue
             for package in args.package:
                 if package in package_list:
-                    print( host + ': ' + package + ' FOUND')
+                    print(host + ': ' + package + ' FOUND')
                 else:
-                    print( host + ': ' + package + ' NOT FOUND')
-
-
-#    package_list = ssh_to_host(sys.argv[1], 'dpkg -l')
-#    print('Packaces found on ' + sys.argv[1])
-#    for package in packages_to_find:
-#        if package in package_list:
-#            print( package + ' FOUND')
-#        else:
-#            print( package + ' NOT FOUND')
+                    print(host + ': ' + package + ' NOT FOUND')
 
 
 if __name__ == '__main__':
-  main()
+    main()
