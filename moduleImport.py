a91f2694eae18003a49a585d5dbbb372235f2f99
bordershot <bordershot@yahoo.com>
2016-05-08 16:21:29 -0700

    added import * message

--- moduleImport.py
+++ moduleImport.py
@@ -11,14 +11,17 @@ from random import *
 
 def main():
     print('Different import types')
-    print('import os')
-    print(str(os.cpu_count()) + ' CPUs, called using "os.cpucount()".')
-    print('from math import pi')
-    print(str(pi) + ', constant called with "pi", no prepend necessary.')
-    print('from random import *')
+    print('import os:')
+    print(str(os.cpu_count()) + ' CPUs, called using "os.cpucount()".\n')
+    print('from math import pi:')
+    print(str(pi) + ', constant called with "pi", no prepend necessary.\n')
+    print('from random import *:')
     print(str(randint(10, 20)) +
-          ', called using "randint(10, 20)", no prepend necessary.')
-    print('import * could be dangerous because of namespace collisions.')
+          ', called using "randint(10, 20)", no prepend necessary.\n')
+    print('import *:')
+    print('Could be dangerous because of namespace collisions.')
+    print('Hard to trace where a function came from.')
+    print('pyflakes can\'t detect statical errors')
 
 if __name__ == '__main__':
     main()
